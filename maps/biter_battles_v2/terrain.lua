9c2c09bbb56eca34d849d5f8f6fb90e71867cd02
Morgan Christiansson <git@mog.se>
2025-03-03 15:03:24 +0100

    Revert "spawn small egg rafts in biter area"
    This reverts commit 026edcecc854eb2fb7155e45700ad5563324b09b.

--- maps/biter_battles_v2/terrain.lua
+++ maps/biter_battles_v2/terrain.lua
@@ -848,7 +848,6 @@ end
 local function populate_biter_area(surface, chunk_pos, rng, is_biter_area_chunk)
     local spitter_spawner_template = { name = 'spitter-spawner', position = { 0, 0 }, force = 'north_biters' }
     local biter_spawner_template = { name = 'biter-spawner', position = { 0, 0 }, force = 'north_biters' }
-    local gleba_spawner_template = { name = 'gleba-spawner-small', position = { 0, 0 }, force = 'north_biters' }
     local worm_turret_template = { name = '', position = { 0, 0 }, force = 'north_biters' }
 
     local left_top_x = chunk_pos.x * 32
@@ -865,12 +864,8 @@ local function populate_biter_area(surface, chunk_pos, rng, is_biter_area_chunk)
         spitter_spawner_template.position[1], spitter_spawner_template.position[2] = x, y
         if (is_biter_area_chunk or is_biter_area(seed, x, y)) and can_place_entity(spitter_spawner_template) then
             local e
-            local r = rng(1, 5)
-            if r == 1 then
+            if rng(1, 4) == 1 then
                 e = create_entity(spitter_spawner_template)
-            elseif r == 2 then
-                gleba_spawner_template.position[1], gleba_spawner_template.position[2] = x, y
-                e = create_entity(gleba_spawner_template)
             else
                 biter_spawner_template.position[1], biter_spawner_template.position[2] = x, y
                 e = create_entity(biter_spawner_template)
