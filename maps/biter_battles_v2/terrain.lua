cc15a424ca51e1c0e26e3ad5bf9e51d641636b39
Morgan Christiansson <git@mog.se>
2025-03-17 04:43:10 +0100

    Allow untrusted users to place landfill when it's not on water/deepwater

--- maps/biter_battles_v2/terrain.lua
+++ maps/biter_battles_v2/terrain.lua
@@ -1275,7 +1275,7 @@ function Public.restrict_landfill(surface, user, tiles)
             if user ~= nil then
                 user.print('You can not landfill the river', { color = { r = 0.22, g = 0.99, b = 0.99 } })
             end
-        elseif user ~= nil and not trusted[user.name] then
+        elseif user ~= nil and not trusted[user.name] and (t.old_tile.name == 'deepwater' or t.old_tile.name == 'water') then
             surface.set_tiles({ { name = t.old_tile.name, position = t.position } }, true)
             user.insert({ name = 'landfill', count = 1 })
             user.print(
